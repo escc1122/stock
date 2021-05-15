6bac262840fb1c1d779b58b7e42c56ccbfab9eb0
escc1122 <escc1122@gamil.com>
2021-05-16 01:27:01 +0800

    排版調整

--- init_yestoday_stock_status.py
+++ init_yestoday_stock_status.py
@@ -5,40 +5,31 @@
 
 import twstock
 import customer_db
-
 import numpy as np
-
 import time
 
 
-
-
-
-# twstock.__update_codes()
-
-# print(twstock.codes)    
-
 stock_array = customer_db.get_stock_ids()
 
-newarr = np.array_split(stock_array, int(len(stock_array)/150)+1)
+newarr = np.array_split(stock_array, int(len(stock_array) / 150) + 1)
 
 # print (newarr[0].tolist())
 
 # # print (stock_array)
 
-error_ids=[]
-no_found_id =[]
+error_ids = []
+no_found_id = []
 
 
-def insertTable(ids):
+def insert_table(ids):
     stocks = twstock.realtime.get(ids.tolist())
-    print(stocks['success'] )
+    print(stocks['success'])
     if stocks['success'] == True:
         for id in ids:
             if id not in twstock.twse:
                 print("test " + id)
-                twstock.twse[id] ={}
-        
+                twstock.twse[id] = {}
+
         customer_db.get_connect()
         conn = customer_db.get_connect()
         cursor = conn.cursor()
@@ -49,20 +40,26 @@ def insertTable(ids):
                 open_price = realtime['open']
                 if latest_trade_price == '-':
                     latest_trade_price = '-1'
-                cursor.execute("INSERT INTO yestoday_stock_status (stock_id, trade_volume, close_price, open_price) VALUES (%s, %s, %s, %s) on conflict (stock_id) do update set trade_volume=%s;", (id,realtime['accumulate_trade_volume'],latest_trade_price,open_price,realtime['accumulate_trade_volume']))
+                cursor.execute(
+                    "INSERT INTO yestoday_stock_status (stock_id, trade_volume, close_price, open_price) VALUES (%s, %s, %s, %s) on conflict (stock_id) do update set trade_volume=%s;",
+                    (id, realtime['accumulate_trade_volume'], latest_trade_price, open_price,
+                     realtime['accumulate_trade_volume']))
             else:
                 print("id no found " + id)
                 no_found_id.append(id)
-                cursor.execute("INSERT INTO yestoday_stock_status (stock_id, trade_volume, close_price, open_price) VALUES (%s, %s, %s, %s) on conflict (stock_id) do update set trade_volume=0;", (id,'-1','-1','-1'))
+                cursor.execute(
+                    "INSERT INTO yestoday_stock_status (stock_id, trade_volume, close_price, open_price) VALUES (%s, %s, %s, %s) on conflict (stock_id) do update set trade_volume=0;",
+                    (id, '-1', '-1', '-1'))
         conn.commit()
         cursor.close()
         conn.close()
     else:
         print("re")
         time.sleep(20)
-        insertTable(ids)
+        insert_table(ids)
 
-def cleanTable():
+
+def clean_table():
     print("cleanTable")
     customer_db.get_connect()
     conn = customer_db.get_connect()
@@ -71,8 +68,10 @@ def cleanTable():
     conn.commit()
     cursor.close()
     conn.close()
-        
-cleanTable()
-for ids in newarr:
-    insertTable(ids)
-    time.sleep(20)
+
+
+if __name__ == '__main__':
+    clean_table()
+    for ids in newarr:
+        insert_table(ids)
+        time.sleep(20)
