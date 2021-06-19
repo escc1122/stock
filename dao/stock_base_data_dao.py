fd0da9d62f0343ec8636b5fad247ace727d3ea4b
escc1122 <escc1122@gamil.com>
2021-06-19 15:47:51 +0800

    update 測試dao

--- dao/stock_base_data_dao.py
+++ dao/stock_base_data_dao.py
@@ -8,10 +8,14 @@ Session = sessionmaker(bind=engine)
 
 def get_stock_base_data_for_key(stock_date, stock_id):
     with Session() as session:
-        session.query(StockBaseData).filter(
+        return session.query(StockBaseData).filter(
             StockBaseData.stock_date == stock_date).filter(StockBaseData.stock_id == stock_id).order_by(
             StockBaseData.stock_id).all()
 
 
 if __name__ == '__main__':
-    get_stock_base_data_for_key('20210619', "2203")
+    stock_base_data_list = get_stock_base_data_for_key('20210617', "5269")
+    print(type(stock_base_data_list))
+    for stock_base_data in stock_base_data_list:
+        print(stock_base_data)
+
